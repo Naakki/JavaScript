616ba95d102a1e5336017d109f3b8bcd1f5d7075
Alex <alex.nakki@gmail.com>
2020-04-27 15:38:50 +0300

    added gradient to colorClock

--- colorClock.js
+++ colorClock.js
@@ -9,12 +9,14 @@ setInterval(function() {
 	let hours = date.getHours();
 	let minutes = date.getMinutes();
 	let seconds = date.getSeconds();
+	let color = '#' + hours + minutes + seconds;
+	let randColor = '#' + Math.floor(Math.random() * 256).toString(16) + Math.floor(Math.random() * 256).toString(16) + Math.floor(Math.random() * 256).toString(16);
 
 	if (hours < 10) {hours = '0' + hours;}
 	if (minutes < 10) {minutes = '0' + minutes;}
 	if (seconds < 10) {seconds = '0' + seconds;}
 
 	time.innerHTML = hours + ':' + minutes + ':' + seconds;
-	colorCode.innerHTML = '#' + hours + minutes + seconds;
-	clock.style.background = '#' + hours + minutes + seconds;
+	colorCode.innerHTML = 'from: ' + color + ' to: ' + randColor;
+	clock.style.background = `linear-gradient(45deg, ${color}, ${randColor})`;
 }, 1000);
